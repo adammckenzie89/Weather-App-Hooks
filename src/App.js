9667a828e8ea48d32442f26259c38034a270309d
adam mckenzie <adammckenzie@adams-MacBook-Pro-3.local>
2019-06-28 13:55:07 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather app with React Hooks!</h1>
-      <h4>Testing</h4>
+      <h4>Testing blah</h4>
     </div>
   );
 }
